c5126b40ca5df9e45f84fa7cbe65951946450c27
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-08-22 12:12:42 +0500

    modifications

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
-
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
+// eslint-disable-next-line
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+  // eslint-disable-next-line
+} from "@syncfusion/ej2-react-charts";
+// eslint-disable-next-line
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+  // eslint-disable-next-line
+} from "../../data/dummy";
+// eslint-disable-next-line
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
   const { currentMode } = useStateContext();
@@ -15,14 +32,20 @@ const Stacked = ({ width, height }) => {
       width={width}
       height={height}
       chartArea={{ border: { width: 0 } }}
+      // eslint-disable-next-line
       tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      legendSettings={{ background: 'white' }}
+      // eslint-disable-next-line
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      // eslint-disable-next-line
+      legendSettings={{ background: "white" }}
     >
       <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+        {/* // eslint-disable-next-line */}
+        {stackedCustomSeries.map((item, index) => (
+          // eslint-disable-next-line
+          <SeriesDirective key={index} {...item} />
+        ))}
       </SeriesCollectionDirective>
     </ChartComponent>
   );
